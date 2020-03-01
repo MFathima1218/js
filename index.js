8b46ecbe7cce3f821c8e1deee79edaccf513f99b
MFathima1218 <fathimamuskan1218@gmail.com>
2020-03-01 08:48:38 +0530

    js-basics

---
+++ index.js
@@ -0,0 +1,55 @@
+
+function goto(){
+    var a=document.getElementById("icon");
+    a.innerHTML="https://www.google.com/search?rlz=1C1CHBD_enIN876IN876&sxsrf=ALeKk01OnWWBavmjLZ3OUs7QY6QqdaR6wQ%3A1583028842188&ei=ahpbXuWOC4WV4-EPnbKwqAg&q=ghousia+college+wikipedia&oq=ghousia+college+wi&gs_l=psy-ab.1.1.33i160l2.16259.19709..22558...0.2..0.335.717.0j1j1j1......0....1..gws-wiz.......0i71j0j0i67j0i22i30.Xu3LL9R1_8Y "  
+}
+
+
+function info(){
+
+// if conditions
+
+if(student>50){
+    console.log("strength is more in class");
+}
+else if(student<50){
+    console.log("less strength");
+}
+else(student==50){
+    console.log("exact strength");
+}
+}
+
+// switch statment 
+
+var grade;
+switch(grade){
+case 'a': console.log("good clg");
+break;
+case 'b': console.log("average");
+break;
+default: console.log("invalid grade");
+}
+
+
+
+//  loops
+
+var i=50;
+for (i=0;i>=x;i++){
+    console.log(i);
+}
+
+var i=1;
+while(i>=0){
+    i++;
+    console.log(i);
+}
+
+var i=1;
+do{
+    i=i+1
+    console.log(i);
+}while(i>=50);
+
+
